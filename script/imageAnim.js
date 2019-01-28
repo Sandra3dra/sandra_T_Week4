ce38bae034fe53386f6322e624496c24dc15395f
Tsao <s_tsao@tss-hr420-dm10.fc.ca>
2019-01-28 16:28:31 -0500

    drag and drop

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,13 +1,29 @@
 (() => {
-	// set up the puzzle pieces and boards
-	let theButton = document.querySelector("#buttonHolder img");
+	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-	// window.addEventListener("load", changeHeaderText);     
+	let piecesBoard = document.querySelector(".puzzle-pieces"),
+		puzzleBoard = document.querySelector(".puzzle-board"),
+		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
 
-	function changeHeaderText(){
-		document.querySelector("h1").textContent = "Free Tacos!"
+	function createPuzzlepieces(pictureIndex){
+		// debugger;
+		pieces.forEach((piece, index) => {
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
+
+			piecesBoard.innerHTML += newPuzzlePiece;
+ 		})
+
+ 		puzzleBoard.style.backgroundImage = `url(./images/backGround${pictureIndex}.jpg)`;
+ 		
 	}
 
-	theButton.addEventListener("click", changeHeaderText);
+	function resetPuzzlePieces(){
+		// debugger;
+		piecesBoard.innerHTML = "";
+		createPuzzlepieces(this.dataset.puzzleref)
+	}
 
+	puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces));
+
+	createPuzzlepieces(0);
 })();
